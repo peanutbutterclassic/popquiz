790beefa4647fe0603c4f9f2557d2e2a0e3329bf
peanutbutterclassic <rano.r.s.fisher@gmail.com>
2022-01-04 18:10:21 +0000

    Add JavaScript link and create startQuiz function to start quiz

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const startButton = document.getElementById("start-btn")
+
+startButton.addEventListener("click", startQuiz)
+
+function startQuiz() {
+    console.log("started")
+}
+
+function getNewQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
